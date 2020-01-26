8998629a6c5f8c134d13e739c4b44a4fb007dd51
Harish Rathor <harishrathor95@gmail.com>
2020-01-26 13:30:37 +0530

    Run query on MongoDB.

--- web-server/src/modules/app/controllers/app/app.controller.js
+++ web-server/src/modules/app/controllers/app/app.controller.js
@@ -1,10 +1,15 @@
-
-
+const DBService = require('@modules/app/services/db.service');
 class AppController {
 
     requestHandlerAction(req, res) {
-        res.json({
-            name: 'Harish Rathor'
+        const dbService = new DBService();
+        dbService.insertUser().then((err, resPonse) => {
+            console.log(resPonse);
+            if (err) {
+                res.send({message: 'Error in insertig user.'});
+            } else {
+                res.send({message: 'User Created.'});
+            }
         });
     }
 }
--- web-server/src/modules/app/services/db.service.js
+++ web-server/src/modules/app/services/db.service.js
@@ -1,5 +1,29 @@
+const { getConnection } = require('@db');
+
 class DBService {
 
+    _runQuery(collectionName, collectionMethod, ...methodArgs) {
+        return new Promise((resolve, reject) => {
+            getConnection().collection(collectionName, (err, collection) => {
+                if (err) {
+                    console.log('Error in getting collection.', err);
+                } else {
+                    if (!methodArgs) {
+                        methodArgs = [];
+                    }
+                    methodArgs.push(resolve);
+                    collection[collectionMethod].apply(collection, methodArgs);
+                }
+            });
+        });
+    }
+
+    insertUser() {
+        return this._runQuery('users', 'insertOne', {
+            firstName: 'Harish',
+            lastName: 'Harish Rathor'
+        });
+    }
 }
 
 module.exports = DBService;
